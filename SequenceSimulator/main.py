306651e742f4b02c0aac482cf28f1b11feca6fe2
Dejan Boskovic <db@infogram.rs>
2021-04-12 17:20:42 +0200

    Pycharm project initialized
    Added a pycharm project with 2 methods so far
    1) method which creates a quality array depending on mean, sigma and the length of the array
    2) method which mutates the reference genome based on insert, delete and snv error rates

---
+++ SequenceSimulator/main.py
@@ -0,0 +1,69 @@
+import random
+import numpy
+
+qualities_in_ascii = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+bases = ['A', 'T', 'C', 'G']
+complementary_bases = {
+    "A": "T",
+    "C": "G",
+    "T": "A",
+    "G": "C"
+}
+
+
+# sigma should probably be 1.0, mean 60-80
+def create_qualities_by_normal_distribution(length, mean, sigma):
+    quality_values = numpy.random.normal(mean, sigma, length)
+    qualities = ''
+    for value in quality_values:
+        if chr(value) in qualities_in_ascii:
+            quality = value
+        else:
+            if ord('!') > value > 0:
+                quality = ord('!')
+            else:
+                if value > ord('~'):
+                    quality = ord('~')
+                else:
+                    raise ValueError("Quality can't be negative, something bad happened with Normal distribution")
+
+        qualities += chr(quality)
+
+    return qualities
+
+
+# keep error rates under 0.01 eg. 0.001, 0.005, 0.009
+
+
+def mutate_genome(reference_genome, insert_error_rate, delete_error_rate, snv_error_rate):
+    genome_length = len(reference_genome)
+    number_of_insertions = round(insert_error_rate * genome_length)
+    number_of_deletions = round(delete_error_rate * genome_length)
+    number_of_variations = round(snv_error_rate * genome_length)
+
+    # introduce single nucleotide variation - SNV
+
+    while number_of_variations > 0:
+        variation_index = random.randint(0, genome_length)
+        base_index = random.randint(0, 100) % 4
+        while base_index == bases.index(reference_genome[variation_index]):
+            base_index = random.randint(0, 100) % 4
+        reference_genome[variation_index] = bases[base_index]
+        number_of_variations -= 1
+    # introduce insertion of singular nucleotides
+
+    while number_of_insertions > 0:
+        insertion_index = random.randint(0, genome_length)
+        base_index = random.randint(0, 100) % 4
+        reference_genome.insert(insertion_index, bases[base_index])
+        genome_length += 1
+        number_of_insertions -= 1
+
+    # introduce deletion of singular nucleotides
+    while number_of_deletions > 0:
+        deletion_index = random.randint(0, genome_length)
+        del reference_genome[deletion_index]
+        genome_length -= 1
+        number_of_deletions -= 1
+
+    return reference_genome
